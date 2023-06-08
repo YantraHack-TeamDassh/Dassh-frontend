faa6cee10ce402ac27c0d04f0d89feceb6499b43
Adrish Srivastava <adrish.srivastava2021@vitstudent.ac.in>
2023-06-08 13:57:32 +0530

    ML API

---
+++ vite-project/src/re.jsx
@@ -0,0 +1,23 @@
+
+const imageFile = document.querySelector('#your-image-input').files[0];
+const reader = new FileReader();
+reader.onloadend = function () {
+  const base64Image = reader.result.split(',')[1];
+  fetch('http://localhost:8000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ image: base64Image }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Process the received data
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+};
+
+reader.readAsDataURL(imageFile);
